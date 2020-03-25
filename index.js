cd6f4ec09c349e82f48db6db4f46c15cd7525a63
Carly Lord <carly.lord@gmail.com>
2020-03-25 01:49:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ function doToElementsInArray(array, callback){
 }// Add your doToElementsInArray() function here:
 
 
-// Add your changeCompletely() function here:
+function changeCompletely(element, index, array){
+array[index] = Math.floor(Math.random()*100)+2).toString() + ` ${array[index]}s!!!`
+}
+
+  
+}// Add your changeCompletely() function here:
